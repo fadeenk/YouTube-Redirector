cc944624d91db217b625ae7b9893294c93a25098
Fadee Kannah <fadeekannah@gmail.com>
2017-07-29 18:24:58 -0700

    Fixed not redirecting in some cases

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-y =$(".yt-uix-button-content")[1].innerText.split(" ")[1].toString(); //look for sign in button
-if( y != "in"){ //if dosent say sign in
+var needToSignIn = document.getElementsByClassName('yt-uix-button-content')[0].innerText === 'Sign in';
+if (!needToSignIn) { //if dosent say sign in
    document.location = "http://www.youtube.com/feed/subscriptions"; //redirect
 }
