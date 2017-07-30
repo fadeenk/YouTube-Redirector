69d5a1fa66677131f121641f128153118f99845f
Fadee Kannah <fadeekannah@gmail.com>
2017-07-29 18:17:53 -0700

    Rewrote the plugin to be more efficent and use synced storage

--- background.js
+++ background.js
@@ -1,48 +1,78 @@
-function checkForValidUrl(tabId, changeInfo, tab) {
-	localStorage.setItem('id',tabId);
-	loged = parseInt(localStorage.getItem('loged'));
-	if(loged == 0){
-		chrome.pageAction.setIcon({tabId: tab.id, path: 'icons/off-48.png'});
-	}
-	else if(loged == 1){
-		chrome.pageAction.setIcon({tabId: tab.id, path: 'icons/on-48.png'});
-		if(tab.url.length < 25){
-      chrome.tabs.executeScript(tab.id, {file: 'script.js'}, function() {
-        console.log('Successfully injected script into the page');
-      });
-		}
-	}
-  if (tab.url.indexOf('youtube') >= 0) {
-    chrome.pageAction.show(tabId);
-  }
-};
+var featureEnabled;
+var openTabs = [];
 
-function install_notice() {
-	var CurVer = chrome.app.getDetails().version;
-	while( CurVer.indexOf('.') > 0 ){
-	    CurVer = CurVer.replace('.', '');
-	}
-	CurVer = parseInt(CurVer);
-	console.log(localStorage.getItem('version'));
-	console.log(CurVer);
-  // if(localStorage.getItem('version') == null){
-	// 	console.log("case null");
-	// 	localStorage.setItem('version',CurVer);
-	// 	chrome.tabs.create({url: "Thankyou.html"});
-  //   }
-	// else if (localStorage.getItem('version') < CurVer){
-	// console.log("case update");
-		chrome.tabs.create({url: "log.txt"});
-		localStorage.setItem('version',CurVer);
-	//}
-}
+checkNewInstallation();
+syncStorageArea();
 
-install_notice();
-if(localStorage.getItem('loged') == null){
-var loged = 0;
-localStorage.setItem('loged',loged);
-}
-else{
-loged = parseInt(localStorage.getItem('loged'));
-};
 chrome.tabs.onUpdated.addListener(checkForValidUrl);
+
+function syncStorageArea() {
+  chrome.storage.sync.get(null, function(storage, err) {
+    if (err) {
+      throw err;
+    }
+    if (storage.featureEnabled === null || storage.featureEnabled === undefined) {
+      storage.featureEnabled = false;
+      saveChanges(storage);
+    }
+    featureEnabled =  storage.featureEnabled;
+  });
+}
+
+function saveChanges(obj) {
+  // Save it using the Chrome extension storage API.
+  chrome.storage.sync.set(obj, function(err) {
+    if (err) throw err;
+    console.log('Settings saved');
+  });
+}
+
+chrome.storage.onChanged.addListener(function(changes, namespace) {
+  for (key in changes) {
+    if (key === 'featureEnabled') {
+      featureEnabled = changes[key].newValue;
+      updatePageActionIcon();
+    }
+  }
+});
+
+function updatePageActionIcon() {
+  var path = featureEnabled ? './icons/on-48.png' : './icons/off-48.png';
+  openTabs.forEach((tabID) => {
+    chrome.pageAction.setIcon({tabId: tabID, path: path});
+  });
+}
+
+function isYoutube(tab) {
+  return tab.url.indexOf('youtube') >= 0;
+}
+
+function checkForValidUrl(tabId, changeInfo, tab) {
+  if (changeInfo.status === 'complete') {
+    if (!isYoutube(tab)) {
+      chrome.pageAction.hide(tabId);
+      return null;
+    }
+    openTabs.push(tabId);
+    updatePageActionIcon();
+    chrome.pageAction.show(tabId);
+    if (featureEnabled && isYoutube(tab)) {
+      if(tab.url.length < 25){
+        chrome.tabs.executeScript(tab.id, {file: 'script.js'}, function() {
+          console.log('Successfully injected script into the page');
+        });
+      }
+    }
+
+  }
+}
+
+function checkNewInstallation() {
+	var CurVer = parseInt(chrome.app.getDetails().version.replace(/\./g, ''));
+	var oldVersion = localStorage.getItem('version');
+	console.log(oldVersion, CurVer);
+  if (!oldVersion || oldVersion < CurVer) {
+		localStorage.setItem('version', CurVer);
+    chrome.tabs.create({url: "log.txt"});
+	}
+}
--- pop.js
+++ pop.js
@@ -1,44 +1,23 @@
-var loged = parseInt(localStorage.getItem('loged'));
 var BGPage = chrome.extension.getBackgroundPage();
-var id = parseInt(localStorage.getItem('id'));
 
-$(document).ready(function () {
-	html();
-});
+var parent = document.querySelector('.toggle-btn');
+var checkbox = parent.querySelector('input.cb-value');
 
+checkbox.checked = BGPage.featureEnabled;
+updateButtonStyles();
+BGPage.updatePageActionIcon();
 
-function html() {
-	console.log('gen');
-
-	if(loged === 0){
-		$('.loged').html('<p>The YouTube Redirector is: <button class="off" id="btn">Off</button>');
-	}
-	else{
-		$('.loged').html('<p>The YouTube Redirector is: <button class="on" id="btn">On</button>');
-	}
-	setTimeout(addListener,100);
-}
-
-function addListener(){
-	document.querySelector('#btn').addEventListener('click', clickHandler);
-}
+document.querySelector('.cb-value').addEventListener('click', clickHandler);
 
 function clickHandler(e) {
-	setTimeout(toggle, 100);
+  updateButtonStyles();
+  BGPage.saveChanges({featureEnabled: checkbox.checked});
 }
 
-function toggle(){
-	console.log('toggole');
-	if(loged == 0){
-		loged = 1;
-		localStorage.setItem('loged',loged);
-	}
-	else{
-		loged = 0;
-		localStorage.setItem('loged',loged);
-	};
-	chrome.tabs.get(id, function(tab){
-		BGPage.checkForValidUrl(tab.id, tab.url, tab);
-	});
-	html();
-};
+function updateButtonStyles() {
+  if (checkbox.checked) {
+    parent.classList.add('active')
+  } else {
+    parent.classList.remove('active');
+  }
+}
